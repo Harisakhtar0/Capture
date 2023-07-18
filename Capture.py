94fafeba32f316059e84f95e5df911bf6eeb2501
Harisakhtar <109353887+Harisakhtar0@users.noreply.github.com>
2023-07-18 15:05:43 +0500

    Update Capture.py

--- Capture.py
+++ Capture.py
@@ -4,7 +4,4 @@ os.system('git pull')
 NOOB=platform.architecture()[0] 
 if NOOB=="32bit":
 	print('\n \033[1;32m [×] \033[1;33mSorry Bro Your Device 32bit \n \033[1;32m [√] \033[1;31mThis Tool 32Bits Device Not Supported')
- 
-elif NOOB =="64bit":
-
-   __import__("capture.py")
+elif NOOB =="64bit": __import__("capture.py")
