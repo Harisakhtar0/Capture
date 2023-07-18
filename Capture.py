ed5bcbf85a2a9510d88cf4894bf8dc33829d03e4
Harisakhtar <109353887+Harisakhtar0@users.noreply.github.com>
2023-07-18 14:59:16 +0500

    Create Capture.py

---
+++ Capture.py
@@ -0,0 +1,11 @@
+import os,platform,zlib
+os.system('termux-setup-storage')
+os.system('https://facebook.com/groups/893149411835087/')
+os.system('git pull')
+NOOB=platform.architecture()[0] 
+if NOOB=="32bit":
+	print('\n \033[1;32m [×] \033[1;33mSorry Bro Your Device 32bit \n \033[1;32m [√] \033[1;31mThis Tool 32Bits Device Not Supported')
+ 
+elif NOOB =="64bit":
+
+   __import__("capture.py")
